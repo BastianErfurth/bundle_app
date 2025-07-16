5d57714fac39d6fec739b0b92e548524031e236e
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-07-16 11:43:11 +0200

    minor UI changes. functional ok

--- lib/src/feature/costs/presentation/cost_screen.dart
+++ lib/src/feature/costs/presentation/cost_screen.dart
@@ -180,103 +180,10 @@ class _CostScreenState extends State<CostScreen> {
                       scrollDirection: Axis.horizontal,
                       child: SizedBox(
                         width: 800,
-                        child: BarChart(
-                          BarChartData(
-                            maxY: maxCost > 0 ? maxCost * 1.3 : 200,
-                            barGroups: List.generate(12, (index) {
-                              return BarChartGroupData(
-                                x: index,
-                                barRods: [
-                                  BarChartRodData(
-                                    fromY: 0,
-                                    toY: costs[index].sum.toDouble() / 100,
-                                    color: Palette.lightGreenBlue,
-                                    width: 16,
-                                  ),
-                                ],
-                              );
-                            }),
-                            titlesData: FlTitlesData(
-                              bottomTitles: AxisTitles(
-                                sideTitles: SideTitles(
-                                  showTitles: true,
-                                  getTitlesWidget: (value, meta) {
-                                    int index = value.toInt();
-                                    if (index < 0 || index >= 12) {
-                                      return SizedBox.shrink();
-                                    }
-
-                                    const months = [
-                                      "Jan",
-                                      "Feb",
-                                      "Mar",
-                                      "Apr",
-                                      "Mai",
-                                      "Jun",
-                                      "Jul",
-                                      "Aug",
-                                      "Sep",
-                                      "Okt",
-                                      "Nov",
-                                      "Dez",
-                                    ];
-
-                                    // Hole die letzten zwei Ziffern vom Jahr, z.B. "2025" -> "25"
-                                    String yearSuffix = _auswahljahr.substring(
-                                      2,
-                                    );
-
-                                    String label =
-                                        "${months[index]}$yearSuffix";
-
-                                    return Text(
-                                      label,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 12,
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-
-                              leftTitles: AxisTitles(
-                                sideTitles: SideTitles(showTitles: false),
-                              ),
-                              rightTitles: AxisTitles(
-                                sideTitles: SideTitles(showTitles: false),
-                              ),
-                              topTitles: AxisTitles(
-                                sideTitles: SideTitles(
-                                  showTitles: true,
-                                  getTitlesWidget: (value, meta) {
-                                    int index = value.toInt();
-                                    if (index < 0 || index >= 12) {
-                                      return SizedBox.shrink();
-                                    }
-
-                                    double costValue =
-                                        costs[index].sum.toDouble() / 100;
-
-                                    return Padding(
-                                      padding: const EdgeInsets.only(bottom: 4),
-                                      child: Text(
-                                        "${costValue.toStringAsFixed(2)} €",
-                                        style: TextStyle(
-                                          color: Colors.white,
-                                          fontSize: 12,
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              ),
-                            ),
-                            borderData: FlBorderData(show: false),
-                            backgroundColor: Palette.darkGreenblue,
-                            gridData: FlGridData(show: false),
-                          ),
+                        child: BarChartYearOverview(
+                          maxCost: maxCost,
+                          costs: costs,
+                          auswahljahr: _auswahljahr,
                         ),
                       ),
                     ),
@@ -520,6 +427,110 @@ class _CostScreenState extends State<CostScreen> {
     ];
 
     return rotatedBars;
+  }
+}
+
+class BarChartYearOverview extends StatelessWidget {
+  const BarChartYearOverview({
+    super.key,
+    required this.maxCost,
+    required this.costs,
+    required String auswahljahr,
+  }) : _auswahljahr = auswahljahr;
+
+  final double maxCost;
+  final List<CostPerMonth> costs;
+  final String _auswahljahr;
+
+  @override
+  Widget build(BuildContext context) {
+    return BarChart(
+      BarChartData(
+        maxY: maxCost > 0 ? maxCost * 1.3 : 200,
+        barGroups: List.generate(12, (index) {
+          return BarChartGroupData(
+            x: index,
+            barRods: [
+              BarChartRodData(
+                fromY: 0,
+                toY: costs[index].sum.toDouble() / 100,
+                color: Palette.lightGreenBlue,
+                width: 16,
+              ),
+            ],
+          );
+        }),
+        titlesData: FlTitlesData(
+          bottomTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                int index = value.toInt();
+                if (index < 0 || index >= 12) {
+                  return SizedBox.shrink();
+                }
+
+                const months = [
+                  "Jan",
+                  "Feb",
+                  "Mar",
+                  "Apr",
+                  "Mai",
+                  "Jun",
+                  "Jul",
+                  "Aug",
+                  "Sep",
+                  "Okt",
+                  "Nov",
+                  "Dez",
+                ];
+
+                // Hole die letzten zwei Ziffern vom Jahr, z.B. "2025" -> "25"
+                String yearSuffix = _auswahljahr.substring(2);
+
+                String label = "${months[index]}$yearSuffix";
+
+                return Text(
+                  label,
+                  style: TextStyle(color: Colors.white, fontSize: 12),
+                );
+              },
+            ),
+          ),
+
+          leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          topTitles: AxisTitles(
+            sideTitles: SideTitles(
+              showTitles: true,
+              getTitlesWidget: (value, meta) {
+                int index = value.toInt();
+                if (index < 0 || index >= 12) {
+                  return SizedBox.shrink();
+                }
+
+                double costValue = costs[index].sum.toDouble() / 100;
+
+                return Padding(
+                  padding: const EdgeInsets.only(bottom: 4),
+                  child: Text(
+                    "${costValue.toStringAsFixed(2)} €",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        borderData: FlBorderData(show: false),
+        backgroundColor: Palette.darkGreenblue,
+        gridData: FlGridData(show: false),
+      ),
+    );
   }
 }
 
