4060b861847b7f8ac7b4a33dc047a70f7bc66dd9
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-07-22 16:27:34 +0200

    Repositorys aktualisiert

--- lib/src/data/firebase_repository.dart
+++ lib/src/data/firebase_repository.dart
@@ -9,8 +9,6 @@ import 'package:bundle_app/src/feature/contracts/domain/extra_contract_informati
 import 'package:bundle_app/src/feature/contracts/domain/user_profile.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart' hide Interval;
-import 'package:intl/intl.dart';
 
 class FirebaseRepository implements DatabaseRepository {
   final fs = FirebaseFirestore.instance;
@@ -264,117 +262,117 @@ class FirebaseRepository implements DatabaseRepository {
     }
   }
 
-  @override
-  Future<Map<DateTime, List<String>>> getEvents() async {
-    final List<Contract> contracts =
-        await getMyContracts(); // Beispielhafte Verwendung
-    final Map<DateTime, List<String>> events = {};
+  // @override
+  // Future<Map<DateTime, List<String>>> getEvents() async {
+  //   final List<Contract> contracts =
+  //       await getMyContracts(); // Beispielhafte Verwendung
+  //   final Map<DateTime, List<String>> events = {};
 
-    for (final contract in contracts) {
-      // Debug: Vertragsstart ausgeben
-      debugPrint(
-        "Vertrag '${contract.keyword}' startet am ${contract.contractRuntime.dt}",
-      );
+  //   for (final contract in contracts) {
+  //     // Debug: Vertragsstart ausgeben
+  //     debugPrint(
+  //       "Vertrag '${contract.keyword}' startet am ${contract.contractRuntime.dt}",
+  //     );
 
-      // 1) Vertragsstart als Event
-      final startDate = contract.contractRuntime.dt;
-      final startDay = DateTime(startDate.year, startDate.month, startDate.day);
-      events.putIfAbsent(startDay, () => []);
-      events[startDay]!.add("Vertragsstart: '${contract.keyword}'");
+  //     // 1) Vertragsstart als Event
+  //     final startDate = contract.contractRuntime.dt;
+  //     final startDay = DateTime(startDate.year, startDate.month, startDate.day);
+  //     events.putIfAbsent(startDay, () => []);
+  //     events[startDay]!.add("Vertragsstart: '${contract.keyword}'");
 
-      // 2) Erste Zahlung als Event (wenn vorhanden)
-      final firstCostDate = contract.contractCostRoutine.firstCostDate;
-      if (firstCostDate != null) {
-        final paymentDay = DateTime(
-          firstCostDate.year,
-          firstCostDate.month,
-          firstCostDate.day,
-        );
-        events.putIfAbsent(paymentDay, () => []);
-        events[paymentDay]!.add("Erste Zahlung: '${contract.keyword}'");
-      }
+  //     // 2) Erste Zahlung als Event (wenn vorhanden)
+  //     final firstCostDate = contract.contractCostRoutine.firstCostDate;
+  //     if (firstCostDate != null) {
+  //       final paymentDay = DateTime(
+  //         firstCostDate.year,
+  //         firstCostDate.month,
+  //         firstCostDate.day,
+  //       );
+  //       events.putIfAbsent(paymentDay, () => []);
+  //       events[paymentDay]!.add("Erste Zahlung: '${contract.keyword}'");
+  //     }
 
-      // 3) Kündigungserinnerung
-      if (contract.contractQuitInterval.isQuitReminderAlertSet &&
-          contract.contractRuntime.isAutomaticExtend) {
-        final reminder = _generateQuitReminder(contract);
-        if (reminder != null) {
-          final reminderDate = reminder['reminderDate'] as DateTime;
-          final reminderDay = DateTime(
-            reminderDate.year,
-            reminderDate.month,
-            reminderDate.day,
-          );
-          events.putIfAbsent(reminderDay, () => []);
-          events[reminderDay]!.add(
-            "Kündigung für '${contract.keyword}' zum ${DateFormat('dd.MM.yyyy').format(reminderDate)}",
-          );
-        }
+  //     // 3) Kündigungserinnerung
+  //     if (contract.contractQuitInterval.isQuitReminderAlertSet &&
+  //         contract.contractRuntime.isAutomaticExtend) {
+  //       final reminder = _generateQuitReminder(contract);
+  //       if (reminder != null) {
+  //         final reminderDate = reminder['reminderDate'] as DateTime;
+  //         final reminderDay = DateTime(
+  //           reminderDate.year,
+  //           reminderDate.month,
+  //           reminderDate.day,
+  //         );
+  //         events.putIfAbsent(reminderDay, () => []);
+  //         events[reminderDay]!.add(
+  //           "Kündigung für '${contract.keyword}' zum ${DateFormat('dd.MM.yyyy').format(reminderDate)}",
+  //         );
+  //       }
 
-        // TODO: implement getEvents
-        throw UnimplementedError();
-      }
-    }
-    return events;
-  }
+  //
+  //       throw UnimplementedError();
+  //     }
+  //   }
+  //   return events;
+  // }
 
-  Map<String, dynamic>? _generateQuitReminder(Contract contract) {
-    final start = contract.contractRuntime.dt;
+  // Map<String, dynamic>? _generateQuitReminder(Contract contract) {
+  //   final start = contract.contractRuntime.dt;
 
-    final contractEnd = DateTime(
-      start.year + contract.contractRuntime.howManyinInterval,
-      start.month,
-      start.day,
-    ).subtract(const Duration(days: 1));
+  //   final contractEnd = DateTime(
+  //     start.year + contract.contractRuntime.howManyinInterval,
+  //     start.month,
+  //     start.day,
+  //   ).subtract(const Duration(days: 1));
 
-    final quitInterval = contract.contractQuitInterval.quitInterval;
+  //   final quitInterval = contract.contractQuitInterval.quitInterval;
 
-    DateTime quitDate;
+  //   DateTime quitDate;
 
-    switch (quitInterval) {
-      case QuitInterval.month:
-        quitDate = DateTime(
-          contractEnd.year,
-          contractEnd.month - contract.contractQuitInterval.howManyInQuitUnits,
-          contractEnd.day,
-        );
-        break;
-      case QuitInterval.week:
-        quitDate = DateTime(
-          contractEnd.year,
-          contractEnd.month,
-          contractEnd.day -
-              contract.contractQuitInterval.howManyInQuitUnits * 7,
-        );
-        break;
-      case QuitInterval.day:
-        quitDate = DateTime(
-          contractEnd.year,
-          contractEnd.month,
-          contractEnd.day - contract.contractQuitInterval.howManyInQuitUnits,
-        );
-        break;
-      case QuitInterval.year:
-        quitDate = DateTime(
-          contractEnd.year - contract.contractQuitInterval.howManyInQuitUnits,
-          contractEnd.month,
-          contractEnd.day,
-        );
-        break;
-    }
+  //   switch (quitInterval) {
+  //     case QuitInterval.month:
+  //       quitDate = DateTime(
+  //         contractEnd.year,
+  //         contractEnd.month - contract.contractQuitInterval.howManyInQuitUnits,
+  //         contractEnd.day,
+  //       );
+  //       break;
+  //     case QuitInterval.week:
+  //       quitDate = DateTime(
+  //         contractEnd.year,
+  //         contractEnd.month,
+  //         contractEnd.day -
+  //             contract.contractQuitInterval.howManyInQuitUnits * 7,
+  //       );
+  //       break;
+  //     case QuitInterval.day:
+  //       quitDate = DateTime(
+  //         contractEnd.year,
+  //         contractEnd.month,
+  //         contractEnd.day - contract.contractQuitInterval.howManyInQuitUnits,
+  //       );
+  //       break;
+  //     case QuitInterval.year:
+  //       quitDate = DateTime(
+  //         contractEnd.year - contract.contractQuitInterval.howManyInQuitUnits,
+  //         contractEnd.month,
+  //         contractEnd.day,
+  //       );
+  //       break;
+  //   }
 
-    final reminderDate = quitDate.subtract(
-      const Duration(days: 10),
-    ); // 10 Tage vor Kündigungsfrist
+  //   final reminderDate = quitDate.subtract(
+  //     const Duration(days: 10),
+  //   ); // 10 Tage vor Kündigungsfrist
 
-    if (reminderDate.isBefore(DateTime.now())) {
-      return null;
-    }
+  //   if (reminderDate.isBefore(DateTime.now())) {
+  //     return null;
+  //   }
 
-    return {
-      'title':
-          "Kündigung vorbereiten für '${contract.keyword}'. Kündigung zum ${DateFormat('dd.MM.yyyy').format(contractEnd)}",
-      'reminderDate': reminderDate,
-    };
-  }
+  //   return {
+  //     'title':
+  //         "Kündigung vorbereiten für '${contract.keyword}'. Kündigung zum ${DateFormat('dd.MM.yyyy').format(contractEnd)}",
+  //     'reminderDate': reminderDate,
+  //   };
+  // }
 }
