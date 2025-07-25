5293f3a16e41c13fa6b3bb4f6ef34f11be425887
Bastian Erfurth <bastian.erfurth01@gmail.com>
2025-07-25 13:31:27 +0200

    bugfixed LoginScreen

--- lib/src/feature/autentification/presentation/login_screen.dart
+++ lib/src/feature/autentification/presentation/login_screen.dart
@@ -25,178 +25,174 @@ class _LogInScreenState extends State<LogInScreen> {
     final auth = context.watch<AuthRepository>();
     context.watch<DatabaseRepository>();
     return Scaffold(
+      resizeToAvoidBottomInset: true,
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(24.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
-                    child: Image.asset(
-                      "assets/images/appicon.png",
-                      height: 120,
-                      width: 120,
-                      fit: BoxFit.cover,
-                    ),
+          child: Column(
+            children: [
+              Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  child: Image.asset(
+                    "assets/images/appicon.png",
+                    height: 120,
+                    width: 120,
+                    fit: BoxFit.cover,
                   ),
                 ),
-                Text("Bundle", style: Theme.of(context).textTheme.titleMedium),
-                SizedBox(height: 20),
-                Text(
-                  "Willkommen !",
-                  style: Theme.of(context).textTheme.displaySmall!.copyWith(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(height: 20),
-                SizedBox(
-                  width: double.infinity,
-                  child: AutofillGroup(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        TextFormFieldWithoutIcon(
-                          autofillHints: [AutofillHints.email],
-                          keyboardType: TextInputType.emailAddress,
-                          controller: _emailController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Bitte gib eine Emailadresse ein";
-                            } else if (!RegExp(
-                              r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
-                            ).hasMatch(value)) {
-                              return "Bitte gib eine gültige Emailadresse ein";
-                            } else if (value.length < 5) {
-                              return "Emailadresse muss mindestens 5 Zeichen lang sein";
-                            } else if (value.length > 50) {
-                              return "Emailadresse darf maximal 50 Zeichen lang sein";
-                            }
+              ),
+              Text("Bundle", style: Theme.of(context).textTheme.titleMedium),
+              SizedBox(height: 20),
+              Text(
+                "Willkommen !",
+                style: Theme.of(
+                  context,
+                ).textTheme.displaySmall!.copyWith(fontWeight: FontWeight.w600),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                width: double.infinity,
+                child: AutofillGroup(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormFieldWithoutIcon(
+                        autofillHints: [AutofillHints.email],
+                        keyboardType: TextInputType.emailAddress,
+                        controller: _emailController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Bitte gib eine Emailadresse ein";
+                          } else if (!RegExp(
+                            r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+                          ).hasMatch(value)) {
+                            return "Bitte gib eine gültige Emailadresse ein";
+                          } else if (value.length < 5) {
+                            return "Emailadresse muss mindestens 5 Zeichen lang sein";
+                          } else if (value.length > 50) {
+                            return "Emailadresse darf maximal 50 Zeichen lang sein";
+                          }
+                          return null;
+                        },
+                        labelText: "Email",
+                        hintText: "Emailadresse eingeben",
+                      ),
+                      SizedBox(height: 24),
+                      TextFieldWithIcon(
+                        keyboardType: TextInputType.visiblePassword,
+                        autofillHints: const [AutofillHints.password],
+                        controller: _passwordController,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return "Bitte gib ein Passwort ein";
+                          } else if (value.length < 6) {
+                            return "Passwort muss mindestens 6 Zeichen lang sein";
+                          } else if (value.length > 10) {
+                            return "Passwort darf maximal 10 Zeichen lang sein";
+                          } else if (value.contains(" ")) {
+                            return "Passwort darf keine Leerzeichen enthalten";
+                          } else
                             return null;
-                          },
-                          labelText: "Email",
-                          hintText: "Emailadresse eingeben",
-                        ),
-                        SizedBox(height: 24),
-                        TextFieldWithIcon(
-                          keyboardType: TextInputType.visiblePassword,
-                          autofillHints: const [AutofillHints.password],
-                          controller: _passwordController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return "Bitte gib ein Passwort ein";
-                            } else if (value.length < 6) {
-                              return "Passwort muss mindestens 6 Zeichen lang sein";
-                            } else if (value.length > 10) {
-                              return "Passwort darf maximal 10 Zeichen lang sein";
-                            } else if (value.contains(" ")) {
-                              return "Passwort darf keine Leerzeichen enthalten";
-                            } else
-                              return null;
-                          },
+                        },
 
-                          labelText: "Passwort",
-                          hintText: "Passwort eingeben",
-                          iconButton: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                _isObscured = !_isObscured;
-                              });
-                            },
-                            icon: Icon(
-                              _isObscured
-                                  ? Icons.visibility_off
-                                  : Icons.visibility,
-                            ),
-                          ),
-                          obscureText: _isObscured,
-                        ),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: TextButton(
-                            onPressed: () {},
-                            child: Text("Passwort vergessen?"),
+                        labelText: "Passwort",
+                        hintText: "Passwort eingeben",
+                        iconButton: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              _isObscured = !_isObscured;
+                            });
+                          },
+                          icon: Icon(
+                            _isObscured
+                                ? Icons.visibility_off
+                                : Icons.visibility,
                           ),
                         ),
-                        SizedBox(
-                          width: double.infinity,
-                          child: FilledButton(
-                            onPressed: () async {
-                              await auth.signInWithEmailAndPassword(
-                                _emailController.text,
-                                _passwordController.text,
-                              );
-                            },
-                            child: Text("Login"),
-                          ),
+                        obscureText: _isObscured,
+                      ),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text("Passwort vergessen?"),
                         ),
-                      ],
-                    ),
+                      ),
+                      SizedBox(
+                        width: double.infinity,
+                        child: FilledButton(
+                          onPressed: () async {
+                            await auth.signInWithEmailAndPassword(
+                              _emailController.text,
+                              _passwordController.text,
+                            );
+                          },
+                          child: Text("Login"),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                SizedBox(height: 20),
-                Row(
-                  spacing: 16,
-                  children: [
-                    Expanded(child: Divider(color: Palette.textWhite)),
-                    Text("Oder mit"),
-                    Expanded(child: Divider(color: Palette.textWhite)),
-                  ],
-                ),
-                SizedBox(height: 20),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    FilledButton.icon(
-                      onPressed: () {},
-                      label: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.apple),
-                            Text("Login with Apple"),
-                          ],
-                        ),
+              ),
+              SizedBox(height: 20),
+              Row(
+                spacing: 16,
+                children: [
+                  Expanded(child: Divider(color: Palette.textWhite)),
+                  Text("Oder mit"),
+                  Expanded(child: Divider(color: Palette.textWhite)),
+                ],
+              ),
+              SizedBox(height: 20),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  FilledButton.icon(
+                    onPressed: () {},
+                    label: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [Icon(Icons.apple), Text("Login with Apple")],
                       ),
                     ),
-                    FilledButton(
-                      onPressed: () {},
-                      child: Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          spacing: 4,
-                          children: [
-                            Image.asset("assets/images/google.png", height: 24),
-                            Text("Login with Google"),
-                          ],
-                        ),
+                  ),
+                  FilledButton(
+                    onPressed: () {},
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        spacing: 4,
+                        children: [
+                          Image.asset("assets/images/google.png", height: 24),
+                          Text("Login with Google"),
+                        ],
                       ),
                     ),
-                  ],
-                ),
-                Spacer(),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  spacing: 8,
-                  children: [
-                    Text("Noch kein Konto?"),
-                    TextButton(
-                      onPressed: () {
-                        setState(() {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => RegistrationScreen(),
-                            ),
-                          );
-                        });
-                      },
-                      child: Text("Registrieren"),
-                    ),
-                  ],
-                ),
-              ],
-            ),
+                  ),
+                ],
+              ),
+              Spacer(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 8,
+                children: [
+                  Text("Noch kein Konto?"),
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => RegistrationScreen(),
+                          ),
+                        );
+                      });
+                    },
+                    child: Text("Registrieren"),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
